1f94e28c088dad3de33114eaaaee72baf68e0328
Olek <up202210478@fe.up.pt>
2023-10-08 22:47:43 +0100

    Initial commit

---
+++ src/main/java/com/example/j2eepract/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.j2eepract;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
