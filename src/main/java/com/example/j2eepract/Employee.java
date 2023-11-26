e70e2c5d7f2543129dfce57935fdcd9808d55330
M4rxman <alex20042207@gmail.com>
2023-11-27 00:58:16 +0200

    Employee.java

---
+++ src/main/java/com/example/j2eepract/Employee.java
@@ -0,0 +1,13 @@
+package com.example.j2eepract;
+
+import jdk.jfr.Unsigned;
+
+public class Employee {
+    long id;
+    static int nextId;
+    String name;
+    String surname;
+    double salary;
+
+
+}
